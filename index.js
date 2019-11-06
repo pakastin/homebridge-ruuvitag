321b1b3749d3f000e59d55ec87d860d7362dcbd2
Juha Lindstedt <juha@pakastin.fi>
2019-11-06 20:33:38 +0200

    Try create ruuvitag history

--- index.js
+++ index.js
@@ -1,24 +1,108 @@
 const ruuvi = require('@pakastin/node-ruuvitag');
 const debug = require('debug')('homebridge-ruuvitag');
+const pngjs = require('pngjs');
 
+let hap;
+let PlatformAccessory;
+let Accessory;
 let Service;
 let Characteristic;
+let UUIDGen;
+let StreamController;
 
 const waitingTags = {};
+const waitingHistoryTags = {};
 const tags = {};
+
+const tagHistory = {};
 
 ruuvi.on('found', tag => {
   tags[tag.id] = tag;
   waitingTags[tag.id] && waitingTags[tag.id](tag);
+  waitingHistoryTags[tag.id] && waitingHistoryTags[tag.id](tag);
   delete waitingTags[tag.id];
+  delete waitingHistoryTags[tag.id];
   debug('found', tag.id);
 });
 
 module.exports = (homebridge) => {
+  hap = homebridge.hap;
+  PlatformAccessory = homebridge.platformAccessory;
+  Accessory = homebridge.hap.Accessory;
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
+  UUIDGen = homebridge.hap.uuid;
+  StreamController = hap.StreamController;
+
+  homebridge.registerPlatform('homebridge-ruuvitag', 'Ruuvitag-history', RuuvitagPlatform, true);
   homebridge.registerAccessory('homebridge-ruuvitag', 'Ruuvitag', Ruuvitag, true);
 };
+
+class RuuvitagPlatform {
+  constructor (log, config, api) {
+    this.log = log;
+    this.config = config;
+    this.api = api;
+
+    api.on('didFinishLaunching', () => this.didFinishLaunching());
+  }
+
+  configureAccessory () {}
+
+  didFinishLaunching () {
+    const { log, config, api } = this;
+    const accessories = [];
+
+    if (!config) {
+      return;
+    }
+
+    const interfaceName = config.interfaceName || '';
+
+    config.cameras && config.cameras.forEach(cameraConfig => {
+      const { name, id } = cameraConfig;
+
+      if (!name || !id) {
+        log('Missing parameters');
+        return;
+      }
+      const uuid = UUIDGen.generate(name);
+      const accessory = new PlatformAccessory(name, uuid, Accessory.Categories.CAMERA);
+      this.cameraSource = new History(hap, config, log, interfaceName);
+      accessory.configureCameraSource(this.cameraSource);
+      accessories.push(accessory);
+
+      const listenTo = (tag) => {
+        if (!tagHistory[tag.id]) {
+          tagHistory[tag.id] = [];
+          tag.on('updated', (data) => {
+            tagHistory[tag.id].push(data);
+          });
+        }
+
+        tag.on('updated', (data) => {
+          this.update(tagHistory[tag.id]);
+        });
+      };
+
+      const tag = tags[this.id];
+
+      if (tag) {
+        listenTo(tag);
+      } else {
+        waitingTags[this.id] = (tag) => {
+          listenTo(tag);
+        };
+      }
+    });
+
+    api.publishCameraAccessories('Ruuvitag-history', accessories);
+  }
+
+  update (history) {
+    this.cameraSource.update(history);
+  }
+}
 
 class Ruuvitag {
   constructor (log, config) {
@@ -26,47 +110,56 @@ class Ruuvitag {
     this.name = config.name;
     this.id = config.id;
     this.config = config;
+    this.services = [];
 
     if (!config.disableTemp) {
       this.tempService = new Service.TemperatureSensor(this.name);
       this.tempService
         .getCharacteristic(Characteristic.CurrentTemperature)
         .setProps({ minValue: -200, maxValue: 200, minStep: 0.01 });
+      this.services.push(this.tempService);
     }
     if (!config.disableHumidity) {
       this.humidityService = new Service.HumiditySensor(this.name);
       this.humidityService
         .getCharacteristic(Characteristic.CurrentRelativeHumidity)
         .setProps({ minValue: 0, maxValue: 100, minStep: 0.5 });
+      this.services.push(this.humidityService);
     }
     this.batteryService = new Service.BatteryService(this.name);
     this.batteryService
       .getCharacteristic(Characteristic.ChargingState)
       .setValue(Characteristic.ChargingState.NOT_CHARGEABLE);
+    this.services.push(this.batteryService);
 
     if (config.heatTrigger) {
       this.heatTriggerService = new Service.ContactSensor(config.heatTrigger.name || this.name, 'heat');
       this.heatTriggerValue = Number(config.heatTrigger.value || 0);
+      this.services.push(this.heatTriggerService);
     }
 
     if (config.coldTrigger) {
       this.coldTriggerService = new Service.ContactSensor(config.coldTrigger.name || this.name, 'cold');
       this.coldTriggerValue = Number(config.coldTrigger.value || 0);
+      this.services.push(this.coldTriggerService);
     }
 
     if (config.motionTrigger) {
       this.motionTriggerService = new Service.MotionSensor(config.motionTrigger.name || this.name);
       this.motionTriggerValue = Number(config.motionTrigger.value || 0);
+      this.services.push(this.motionTriggerService);
     }
 
     if (config.highHumidityTrigger) {
       this.highHumidityTriggerService = new Service.ContactSensor(config.highHumidityTrigger.name || this.name, 'highHumidity');
       this.highHumidityTriggerValue = Number(config.highHumidityTrigger.value || 0);
+      this.services.push(this.highHumidityTriggerService);
     }
 
     if (config.lowHumidityTrigger) {
       this.lowHumidityTriggerService = new Service.ContactSensor(config.lowHumidityTrigger.name || this.name, 'lowHumidity');
       this.lowHumidityTriggerValue = Number(config.lowHumidityTrigger.value || 0);
+      this.services.push(this.lowHumidityTriggerService);
     }
 
     const listenTo = (tag) => {
@@ -152,39 +245,78 @@ class Ruuvitag {
   }
 
   getServices () {
-    const services = [];
+    return this.services;
+  }
+}
 
-    if (this.tempService) {
-      services.push(this.tempService);
+const streamOptions = {
+  proxy: false,
+  srtp: true,
+  video: {
+    resolutions: [
+      [1920, 1080, 1]
+    ],
+    codec: {
+      profiles: [0, 1, 2],
+      levels: [0, 1, 2]
     }
+  },
+  audio: {
+    codecs: [
+      {
+        type: 'OPUS',
+        samplerate: 24
+      },
+      {
+        type: 'AAC-eld',
+        samplerate: 16
+      }
+    ]
+  }
+};
 
-    if (this.humidityService) {
-      services.push(this.humidityService);
+class History {
+  constructor (hap, config, log, interfaceName) {
+    this.history = [];
+    this.hap = hap;
+    this.config = config;
+    this.log = log;
+    this.interfaceName = interfaceName;
+    this.controlService = new Service.CameraControl();
+    this.services = [this.controlService];
+    this.maxWidth = 1920;
+    this.maxHeight = 1080;
+    this.streamControllers = [];
+
+    for (let i = 0; i < 2; i++) {
+      const streamController = new StreamController(i, streamOptions, this);
+
+      this.services.push(streamController.service);
+      this.streamControllers.push(streamController);
     }
+  }
 
-    services.push(this.batteryService);
+  update (history) {
+    this.history = history;
+  }
 
-    if (this.heatTriggerService) {
-      services.push(this.heatTriggerService);
-    }
+  handleCloseConnection (connectionID) {
+    this.log('Closing connection', connectionID);
+  }
 
-    if (this.coldTriggerService) {
-      services.push(this.coldTriggerService);
-    }
+  handleSnapshotRequest (request, callback) {
+    this.log('snapshot request', request);
+    callback();
+  }
 
-    if (this.highHumidityTriggerService) {
-      services.push(this.highHumidityTriggerService);
-    }
+  prepareStream (request, callback) {
+    this.log('prepare stream', request);
 
-    if (this.lowHumidityTriggerService) {
-      services.push(this.lowHumidityTriggerService);
-    }
+    callback();
+  }
 
-    if (this.motionTriggerService) {
-      services.push(this.motionTriggerService);
-    }
-
-    return services;
+  handleStreamRequest (request) {
+    this.log('handle stream request', request);
   }
 }
 
